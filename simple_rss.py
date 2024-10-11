bf391ae972d8cc14dcf3f1a0205a668d7b9d0bff
Mahmoud Elbisary <ge42pur@tum.de>
2024-10-12 00:47:36 +0200

    Finished both 'simple_rss.py' for column vector x and 'rss.py' for x as a matrix

--- simple_rss.py
+++ simple_rss.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_synthetic_data(beta):
-    x_1 = np.random.random([1000, beta.shape[0] - 1]) *10
+def generate_synthetic_data(b_1 = 3, b_0 = 5):
+    x_1 = np.random.rand(1000)
     epsilon = (np.random.rand(1000) -0.5 ) *3
-    x = np.column_stack((x_1, np.ones(x_1.shape[0]).T)) 
-    y = x @ beta + epsilon.T
+    x = np.column_stack((np.transpose(x_1 * 10), np.ones(x_1.shape).T)) 
+    y = b_1 * x[:,0] +(b_0 * np.ones(1000)).T + epsilon.T
     return x, y
 
 def optimize_coeffecients(x, y):
@@ -19,7 +19,6 @@ def set_line(b, points):
     plt.scatter(points[0][:,0], points[1])
     plt.show()
 
-
-beta = np.array([3,5,2,9,1])
-x, y = generate_synthetic_data(beta)
-print(optimize_coeffecients(x,y))
+x,y = generate_synthetic_data()
+b = optimize_coeffecients(x, y)
+set_line(b, (x,y))
